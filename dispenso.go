b9b3f93f5e0216b9511bc6d85810c4356eda9802
Robin Verlangen <robin@us2.nl>
2014-04-28 21:42:29 +0200

    Cleanup

--- dispenso.go
+++ dispenso.go
@@ -5,12 +5,20 @@ package main
 import (
 	"log"
 	"flag"
+	"fmt"
 )
 
-// Config through flags
+// Constants
+const defaultPort int = 8011
+
+// Configuration
 var seedNodes string
+var serverPort int
+
+// Set configuration from flags
 func init() {
 	flag.StringVar(&seedNodes, "seeds", "", "Seed nodes, comma separated host:port tuples (e.g. 12.34.56.78,23.34.45.56:8080")
+	flag.IntVar(&serverPort, "port", defaultPort, fmt.Sprintf("Port to bind on (defaults to %d)", defaultPort))
 	flag.Parse()
 }
 
