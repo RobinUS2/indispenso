f8583eb2de654612c916c34f32e3ae07398445a3
Robin Verlangen <robin@us2.nl>
2014-04-28 21:59:38 +0200

    Begin with discovery entities

--- discovery.go
+++ discovery.go
@@ -8,3 +8,24 @@ type Node struct {
 	Host string // Fully qualified hostname
 	Port int // Port on which Dispenso runs
 }
+
+// Message (payload transmitted between nodes containing instructions)
+type Message struct {
+	Type MessageType
+}
+
+// Message types
+type messageType int
+const (
+	discoveryPing	messageType = iota+1 // Initial discovery ping
+	disocveryResponse // Discovery response
+	discoveryMeta // Metadata beyond initial discovery
+	configuration // Used to update configuration in the cluster
+	taskRequest // New task submission
+	taskApproval // Approve task
+	taskReject // Reject task
+	taskExecution // After being approved a task execution will be sent to the nodes
+)
+type MessageType struct {
+	code messageType
+}
