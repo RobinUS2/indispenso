62028fa9340799b51b11b3f3cdb62c382047c490
Robin Verlangen <robin@us2.nl>
2014-04-28 21:39:08 +0200

    Ignore binary

--- dispenso.go
+++ dispenso.go
@@ -3,9 +3,17 @@ package main
 // Imports
 import (
 	"log"
+	"flag"
 )
+
+// Config through flags
+var seedNodes string
+func init() {
+	flag.StringVar(&seedNodes, "seeds", "", "Seed nodes, comma separated host:port tuples (e.g. 12.34.56.78,23.34.45.56:8080")
+	flag.Parse()
+}
 
 // Main function of dispenso
 func main() {
-	log.Println("Hello World")
+	log.Println("Starting dispenso")
 }
