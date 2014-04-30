dea20173fdfc67f20dc6f46798f00704bcf57980
Robin Verlangen <robin@us2.nl>
2014-04-30 09:47:34 +0200

    Raft test

--- dispenso.go
+++ dispenso.go
@@ -8,6 +8,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"github.com/goraft/raft"
 )
 
 // Constants
@@ -23,9 +24,9 @@ var shutdown chan bool = make(chan bool)
 
 // Set configuration from flags
 func init() {
-	flag.StringVar(&seedNodes, "seeds", "", "Seed nodes, comma separated host:port tuples (e.g. 12.34.56.78,23.34.45.56:8080")
-	flag.IntVar(&serverPort, "port", defaultPort, fmt.Sprintf("Port to bind on (defaults to %d)", defaultPort))
-	flag.BoolVar(&storeState, "store-state", true, "Allow to store cluster state on this node (default=true)")
+	flag.Stringvar(&seedNodes, "seeds", "", "Seed nodes, comma separated host:port tuples (e.g. 12.34.56.78,23.34.45.56:8080")
+	flag.Intvar(&serverPort, "port", defaultPort, fmt.Sprintf("Port to bind on (defaults to %d)", defaultPort))
+	flag.Boolvar(&storeState, "store-state", true, "Allow to store cluster state on this node (default=true)")
 	flag.Parse()
 }
 
