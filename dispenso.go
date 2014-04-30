684f9f7f5af99d4ce68637e7a10ac90ef29db7ec
Robin Verlangen <robin@us2.nl>
2014-04-30 15:59:34 +0100

    Cleanup

--- dispenso.go
+++ dispenso.go
@@ -7,8 +7,8 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/goraft/raft"
 	"log"
+	"strings"
 )
 
 // Constants
@@ -24,15 +24,22 @@ var shutdown chan bool = make(chan bool)
 
 // Set configuration from flags
 func init() {
-	flag.Stringvar(&seedNodes, "seeds", "", "Seed nodes, comma separated host:port tuples (e.g. 12.34.56.78,23.34.45.56:8080")
-	flag.Intvar(&serverPort, "port", defaultPort, fmt.Sprintf("Port to bind on (defaults to %d)", defaultPort))
-	flag.Boolvar(&storeState, "store-state", true, "Allow to store cluster state on this node (default=true)")
+	flag.StringVar(&seedNodes, "seeds", "", "Seed nodes, comma separated host:port tuples (e.g. 12.34.56.78,23.34.45.56:8080")
+	flag.IntVar(&serverPort, "port", defaultPort, fmt.Sprintf("Port to bind on (defaults to %d)", defaultPort))
+	flag.BoolVar(&storeState, "store-state", true, "Allow to store cluster state on this node (default=true)")
 	flag.Parse()
 }
 
 // Main function of dispenso
 func main() {
 	log.Println("INFO: Starting dispenso")
+
+	// Start discovery
+	var disco *DiscoveryService = NewDiscoveryService()
+	if len(strings.TrimSpace(seedNodes)) > 0 {
+		disco.SetSeeds(strings.Split(seedNodes, ","))
+	}
+	disco.Start()
 
 	// Start server
 	var server *Server = NewServer()
