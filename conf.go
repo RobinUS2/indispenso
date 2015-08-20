25ddeef34c2a84cc9e84e959d1fb85480fb6c6e2
Robin Verlangen <robin+github@us2.nl>
2015-08-20 08:19:19 +0200

    Begin

---
+++ conf.go
@@ -0,0 +1,10 @@
+package main
+
+// Configuration
+type Conf struct {
+
+}
+
+func newConf() *Conf {
+	return &Conf{}
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+// @author Robin Verlangen
+// Indispenso: Distribute, manage, regulate, arrange. Simple & secure management based on consensus.
+
+var conf *Conf
+
+func main() {
+	conf = newConf()
+}
