8896863417a5286810ab71c243b53bad4c0fcfe9
Robin Verlangen <robin+github@us2.nl>
2015-08-20 10:55:46 +0200

    Two params

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
+import (
+	"flag"
+	"log"
+)
+
 // @author Robin Verlangen
 // Indispenso: Distribute, manage, regulate, arrange. Simple & secure management based on consensus.
 
 var conf *Conf
+var isServer *bool
+var seedUri *string
 
 func main() {
 	conf = newConf()
+
+	// Read flags
+	isServer = flag.Bool("server", false, "Should this run the server process")
+	seedUri = flag.String("seed", "", "Seed URI")
+	flag.Parse()
+	log.Printf("%t", *isServer)
+	log.Printf("%s", *seedUri)
 }
