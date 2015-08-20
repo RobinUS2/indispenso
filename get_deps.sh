22c136b451b84f3095c3af108ed4bd1034f6b410
Robin Verlangen <robin+github@us2.nl>
2015-08-20 08:11:53 +0200

    Remove master content for new version

--- get_deps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export GOPATH=`pwd`
-go get "github.com/dgryski/dgoogauth"
-go get "github.com/gorilla/sessions"
-go get "github.com/nu7hatch/gouuid"
-go get "github.com/pmylund/go-cache"
