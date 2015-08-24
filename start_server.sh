c8609a41bf568fa8d2793e892330ef738d7810c8
Robin Verlangen <robin+github@us2.nl>
2015-08-24 09:19:06 +0200

    Update start server script

--- start_server.sh
+++ start_server.sh
@@ -3,18 +3,17 @@
 mkdir -p /etc/indispenso
 
 # This will start the server of indispenso and will generate a unique token the first time
-SECURE_TOKEN_FILE="server.token"
-if [ ! -f $SECURE_TOKEN_FILE ]; then
+if [ ! -f "/etc/indispenso/indispenso.conf" ]; then
 	echo "Server has no secure token yet, generating token"
 	TOKEN=`openssl rand -base64 128 | tr -d '\n'`
-	echo $TOKEN > $SECURE_TOKEN_FILE
-	echo "Your token is (make sure to store this):"
+	echo "Your token is:"
 	echo "$TOKEN"
 
 	# Update conf file
+	touch /etc/indispenso/indispenso.conf
 	cat /etc/indispenso/indispenso.conf | grep -v "secure_token" > /etc/indispenso/indispenso.conf.bak
 	echo "secure_token: $TOKEN" >> /etc/indispenso/indispenso.conf.bak
-	echo "server: true" >> /etc/indispenso/indispenso.conf.bak
+	echo "server_enabled: true" >> /etc/indispenso/indispenso.conf.bak
 	mv /etc/indispenso/indispenso.conf.bak /etc/indispenso/indispenso.conf
 fi
 
