428d8342d96f51ffe2944c492165820c8647744a
Robin Verlangen <robin+github@us2.nl>
2015-08-27 20:42:34 +0200

    Generate ID

--- execution_validation.go
+++ execution_validation.go
@@ -1,8 +1,13 @@
 package main
 
+import (
+	"github.com/nu7hatch/gouuid"
+)
+
 // Validates the execution of a process
 
 type ExecutionValidation struct {
+	Id           string // Unique id
 	Fatal        bool   // If matched, should we abort the (sequence of) operation(s)?
 	MustContain  bool   // Should this be in there?
 	OutputStream int    // 1 = standard output, 2 error output
@@ -21,7 +26,11 @@ func newExecutionValidation(txt string, fatal bool, mustContain bool, outputStre
 		return nil
 	}
 
+	// Id
+	id, _ := uuid.NewV4()
+
 	return &ExecutionValidation{
+		Id:           id.String(),
 		Fatal:        true,
 		MustContain:  true,
 		Text:         txt,
