438abfc16aa6e01979e0ea72537fefa0b15a6b64
Robin Verlangen <robin+github@us2.nl>
2015-08-27 13:47:39 +0200

    Fix multiple tags

--- console/app.js
+++ console/app.js
@@ -703,7 +703,12 @@ var app = {
 				});
 
 				$('form#create-template').submit(function() {
-					var d = $(this).serialize();
+					var data = $(this).serializeArray();
+					var d = {};
+					for (var k in data) {
+						var v = data[k];
+						d[k['name']] = v['value'];
+					}
 					try { d['includedTags'] = $('#includedTags', app.pageInstance()).val().join(','); } catch (e) {}
 					try { d['excludedTags'] = $('#excludedTags', app.pageInstance()).val().join(','); } catch (e) {}
 					app.ajax('/template', { method: 'POST', data : d }).done(function(resp) {
