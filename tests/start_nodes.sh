e590344725784493e99898ff7fd1a87106b8ff51
Robin Verlangen <robin@us2.nl>
2015-08-30 10:15:44 +0200

    Fix

--- tests/start_nodes.sh
+++ tests/start_nodes.sh
@@ -8,13 +8,12 @@ git pull
 TOKEN=`cat ../server.token`
 echo "Token from file $TOKEN"
 
-# Start the server
-../start_server.sh --debug=true &
-sleep 1
-
 # Start another few clients
 ../indispenso --seed="https://127.0.0.1:897/" --hostname="client-one" --debug=true &
 ../indispenso --seed="https://127.0.0.1:897/" --hostname="client-two" --debug=true &
+
+# Start the server
+../start_server.sh --debug=true &
 
 # Make sure we wait (without wasting cpu cycles), we read "nothing" :)
 cat
