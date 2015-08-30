c899c2c75941304ce8f284d500e6f416987dfac6
Robin Verlangen <robin@us2.nl>
2015-08-30 10:36:39 +0200

    No no cache

--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm -f indispenso_tests
-docker build --tag=indispenso_tests --no-cache .
+docker build --tag=indispenso_tests .
 docker run --name indispenso_tests indispenso_tests
