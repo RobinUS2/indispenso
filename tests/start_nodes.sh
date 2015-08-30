fab1739e2a4fdcef83537d63d467f29e6d125411
Robin Verlangen <robin@us2.nl>
2015-08-30 10:22:38 +0200

    Server first

--- tests/start_nodes.sh
+++ tests/start_nodes.sh
@@ -6,12 +6,13 @@ git pull
 # Build
 ../build.sh
 
+# Start the server
+../start_server.sh --debug=true &
+sleep 3
+
 # Start another few clients
 ../indispenso --seed="https://127.0.0.1:897/" --hostname="client-one" --debug=true &
 ../indispenso --seed="https://127.0.0.1:897/" --hostname="client-two" --debug=true &
-
-# Start the server
-../start_server.sh --debug=true &
 
 # Make sure we wait (without wasting cpu cycles), we read "nothing" :)
 cat
