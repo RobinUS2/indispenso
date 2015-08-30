4f48e433d3fd38fc3fb509640f57862ff65cee30
Robin Verlangen <robin@us2.nl>
2015-08-30 09:53:47 +0200

    3 clients

--- tests/start_nodes.sh
+++ tests/start_nodes.sh
@@ -9,8 +9,10 @@ git pull
 # Start the server
 ../start_server.sh &
 
-# Start another client
-../indispenso --server=false &
+# Start another few clients
+../indispenso --server=false --hostname=client-one &
+../indispenso --server=false --hostname=client-two &
+../indispenso --server=false --hostname=client-three &
 
 # Make sure we wait (without wasting cpu cycles), we read "nothing" :)
 cat
