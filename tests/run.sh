793e4114e72c5e49a0301f4439428e28c08246b2
Robin Verlangen <robin@us2.nl>
2015-08-30 10:37:34 +0200

    Expose interface

--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm -f indispenso_tests
 docker build --tag=indispenso_tests .
-docker run --name indispenso_tests indispenso_tests
+docker run --name indispenso_tests -p 897 indispenso_tests
