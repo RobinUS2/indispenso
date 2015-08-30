15811ee852fa782e52d66c60ddfa9b2bcb0cc115
Robin Verlangen <robin@us2.nl>
2015-08-30 10:20:08 +0200

    Fix

--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker rm -f indispenso_tests
-docker build --tag=indispenso_tests .
+docker build --tag=indispenso_tests --no-cache .
 docker run --name indispenso_tests indispenso_tests
--- tests/start_nodes.sh
+++ tests/start_nodes.sh
@@ -5,8 +5,6 @@ git pull
 
 # Build
 ../build.sh
-TOKEN=`cat ../server.token`
-echo "Token from file $TOKEN"
 
 # Start another few clients
 ../indispenso --seed="https://127.0.0.1:897/" --hostname="client-one" --debug=true &
