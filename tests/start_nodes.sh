54c00ff08bff58a863fdc8bc05773ca55a968ce4
Robin Verlangen <robin@us2.nl>
2015-11-08 15:25:06 +0100

    Better timeout

--- tests/start_nodes.sh
+++ tests/start_nodes.sh
@@ -15,7 +15,11 @@ sleep 3
 ../indispenso --seed="https://127.0.0.1:897/" --hostname="client-two" --debug="true" --disable-server="true" --client-port="-1" >>/out.log 2>&1 &
 
 # Shutdown after 30 seconds
-$(sleep 30 && killall indispenso)
+{
+    sleep 30
+    killall indispenso
+    kill $$
+} &
 
 # Read output
 tail -f /out.log
