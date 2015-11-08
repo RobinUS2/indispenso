758078d9ec4aaa6430249071997575be19bcf407
Robin Verlangen <robin@us2.nl>
2015-11-08 15:13:14 +0100

    All quotes

--- tests/start_nodes.sh
+++ tests/start_nodes.sh
@@ -7,12 +7,12 @@ git pull
 ../build.sh
 
 # Start the server
-../start_server.sh --debug=true --client-port="-1" >>/out.log 2>&1 &
+../start_server.sh --debug="true" --client-port="-1" >>/out.log 2>&1 &
 sleep 3
 
 # Start another few clients
-../indispenso --seed="https://127.0.0.1:897/" --hostname="client-one" --debug=true --disable-server=true >>/out.log 2>&1 &
-../indispenso --seed="https://127.0.0.1:897/" --hostname="client-two" --debug=true --disable-server=true --client-port="-1" >>/out.log 2>&1 &
+../indispenso --seed="https://127.0.0.1:897/" --hostname="client-one" --debug="true" --disable-server="true" >>/out.log 2>&1 &
+../indispenso --seed="https://127.0.0.1:897/" --hostname="client-two" --debug="true" --disable-server="true" --client-port="-1" >>/out.log 2>&1 &
 
 # Make sure we wait (without wasting cpu cycles), we read "nothing" :)
 tail -f /out.log
